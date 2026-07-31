242a2e242760addc55c4468865a82abc2ffe4454
Kiran <121487651+Kirankulal10@users.noreply.github.com>
2026-07-31 15:15:26 +0530

    Change instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_instance" "app_server" {
   ami = "ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "app-server"
